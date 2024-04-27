a96238d844c79ff3161642a057322b4a72053a88
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-04-27 13:18:48 +0200

    pre-run terminal config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use crossterm::execute;
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use ratatui::backend::CrosstermBackend;
+use ratatui::Terminal;
+use std::error::Error;
+use std::io;
+
+use crate::app::App;
+
 mod app;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!!");
+
+    //setup terminal
+    enable_raw_mode()?;
+    let mut stderr = io::stderr();
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
 }
