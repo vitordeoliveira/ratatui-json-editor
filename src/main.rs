6af2868764cec4c36742b4e53b64f23d8e780cb8
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-04-27 13:19:17 +0200

    state creating and loop starting

--- src/main.rs
+++ src/main.rs
@@ -19,4 +19,31 @@ fn main() -> Result<(), Box<dyn Error>> {
     enable_raw_mode()?;
     let mut stderr = io::stderr();
     execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+
+    // State creation and loop starting
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let mut app = App::new();
+    let res = run_app(&mut terminal, &mut app);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Ok(do_print) = res {
+        if do_print {
+            app.print_json();
+        } else if let Err(err) = res {
+            println!("{err:?}");
+        }
+    }
+
+    Ok(())
 }
